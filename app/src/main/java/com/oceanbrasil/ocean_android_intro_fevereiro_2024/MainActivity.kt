9395bfd3e4390be4ccf98e2118cbf183c441c143
Paulo Salvatore <salvatoregames@gmail.com>
2024-02-02 11:07:50 -0300

    Initial commit

---
+++ app/src/main/java/com/oceanbrasil/ocean_android_intro_fevereiro_2024/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oceanbrasil.ocean_android_intro_fevereiro_2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ocean_Android_Intro_Fevereiro_2024"
+include(":app")
